b4a60e07875262f351c2cd8576783d5f45f9df18
hadsunn <59596048+hadsunn@users.noreply.github.com>
2025-09-01 20:41:30 +0300

    Update cupcake.theme.sh

--- themes/cupcake/cupcake.theme.sh
+++ themes/cupcake/cupcake.theme.sh
@@ -66,7 +66,7 @@ function _omb_theme_PROMPT_COMMAND() {
   PS1='\n'$icon_start$(_omb_prompt_print_python_venv)
   PS1+=$icon_user$_omb_prompt_bold_brown'\u'
   PS1+=$_omb_prompt_normal$icon_host$_omb_prompt_bold_teal'\h'
-  PS1+=$_omb_prompt_normal$icon_directory$_omb_prompt_bold_purple'\W'
+  PS1+=$_omb_prompt_normal$icon_directory$_omb_prompt_bold_purple'\w'
   PS1+=$_omb_prompt_normal$([[ -n $(_omb_prompt_git branch 2> /dev/null) ]] && _omb_util_print " on $icon_branch  ")
   PS1+=$_omb_prompt_white$(scm_prompt_info)$_omb_prompt_normal'\n'$icon_end
   PS2=$icon_end
